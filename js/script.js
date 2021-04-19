7dea365eca1e130fa53213d2d950804275b3e3b0
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-04-19 15:45:00 +0200

    milestone 2: multi search (movie and tv)

--- js/script.js
+++ js/script.js
@@ -3,23 +3,35 @@ function initVue() {
         el: '#app',
         data: {
             movie: [],
+            tvseries: [],
             inputSearch: '2021',
         },
         methods:  {
             searchMovie: function() {
-                axios.get('https://api.themoviedb.org/3/search/movie', {
+                axios.get('https://api.themoviedb.org/3/search/multi', {
                     params: {
                         'api_key': 'd03fa56773fc8de0b8df06270921b491',
                         'query': this.inputSearch,
                     }
+    
                 })
             .then(data => {
                 console.log(data);
                 console.log(this.inputSearch);
                 this.movie = data.data.results;
                 console.log(movie);
-                })
+                });
 
+            // axios.get('https://api.themoviedb.org/3/search/tv', {
+            //     params: {
+            //         'api_key': 'd03fa56773fc8de0b8df06270921b491',
+            //         'query': this.inputSearch,
+            //     }    
+            //     })
+            // .then(data => {
+            //     console.log(data);
+            //     this.tvseries = data.data.results;
+            // })
             },
 
         },
